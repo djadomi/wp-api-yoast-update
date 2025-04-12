e8a1b51b5c95a8196e26f2dd750b7a8285e72207
dom <dja@domi.co.uk>
2025-04-12 16:12:17 +0000

    looks like WP doesn't like timestamp version numbers

--- wp-api-yoast-update.php
+++ wp-api-yoast-update.php
@@ -3,7 +3,7 @@
 Plugin Name: WP API Yoast Update
 Plugin URI: https://github.com/yourusername/wp-api-yoast-update
 Description: Updates Yoast SEO fields for posts via WP API.
-Version: 1.0.0
+Version: 0.0.1
 Author: Your Name
 Author URI: https://yourwebsite.com
 GitHub Plugin URI: https://github.com/djadomi/wp-api-yoast-update
